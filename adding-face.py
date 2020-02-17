3c2412a4c647fac1befc34add8f46dc02cde9241
klupzor <lopezgand@gmail.com>
2020-02-17 16:31:32 -0500

    initial commit

---
+++ adding-face.py
@@ -0,0 +1,46 @@
+import boto3
+from config import *
+
+
+def add_faces_to_collection(photo, collection_id):
+
+    client = boto3.client('rekognition',
+                          aws_access_key_id=access_key_id,
+                          aws_secret_access_key=secret_access_key)
+
+    with open(photo, 'rb') as sourceImage:
+        sourceBytes = sourceImage.read()
+
+    response = client.index_faces(CollectionId=collection_id,
+                                  Image={'Bytes': sourceBytes},
+                                  ExternalImageId="hola.jpg",
+                                  MaxFaces=1,
+                                  QualityFilter="AUTO",
+                                  DetectionAttributes=['ALL'])
+
+    print('Results for ' + photo)
+    print('Faces indexed:')
+    for faceRecord in response['FaceRecords']:
+        print('  Face ID: ' + faceRecord['Face']['FaceId'])
+        print('  Location: {}'.format(faceRecord['Face']['BoundingBox']))
+
+    print('Faces not indexed:')
+    for unindexedFace in response['UnindexedFaces']:
+        print(' Location: {}'.format(
+            unindexedFace['FaceDetail']['BoundingBox']))
+        print(' Reasons:')
+        for reason in unindexedFace['Reasons']:
+            print('   ' + reason)
+    return len(response['FaceRecords'])
+
+
+def main():
+    collection_id = 'demo-collection'
+    photo = addPhoto
+
+    indexed_faces_count = add_faces_to_collection(photo, collection_id)
+    print("Faces indexed count: " + str(indexed_faces_count))
+
+
+if __name__ == "__main__":
+    main()
---
+++ find-face.py
@@ -0,0 +1,31 @@
+import boto3
+from config import *
+
+if __name__ == "__main__":
+
+    collectionId = 'demo-collection'
+    # photo = './images/faces.png'
+    photo = comparePhoto
+    threshold = 70
+    maxFaces = 2
+
+    client = boto3.client('rekognition',
+                          aws_access_key_id=access_key_id,
+                          aws_secret_access_key=secret_access_key)
+    with open(photo, 'rb') as sourceImage:
+        sourceBytes = sourceImage.read()
+
+    response = client.search_faces_by_image(CollectionId=collectionId,
+                                            Image={'Bytes': sourceBytes},
+                                            FaceMatchThreshold=threshold,
+                                            MaxFaces=maxFaces)
+    
+    print("response: ")
+    print(response)
+
+    faceMatches = response['FaceMatches']
+    print('Matching faces')
+    for match in faceMatches:
+        print('FaceId:' + match['Face']['FaceId'])
+        print('Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
+        print
